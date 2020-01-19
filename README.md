6106c7c6a83f765858448311d04c431deb3c8e77
Márcio Medeiros <marciomed07@gmail.com>
2020-01-19 01:33:40 -0300

    Walking and Idling animation for 8 directions

---
+++ README.md
@@ -0,0 +1,6 @@
+# A-Dungeon-and-no-Dragon
+
+A top down rogue light game about exploring a dungeon using Godot.
+
+* Godot template: https://github.com/godotengine/godot-demo-projects/tree/3.1-c34a2b4/2d/kinematic_collision
+* Sprites: https://opengameart.org/content/8-directional-character-template
--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # move_and_slide works.
 
 # Member variables
-const MOTION_SPEED = 160 # Pixels/second
+const MOTION_SPEED = 100 # Pixels/second
 
 
 func _physics_process(_delta):
@@ -19,6 +19,36 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("move_right"):
 		motion += Vector2(1, 0)
 	
+	decideAnimation(motion);
+	
 	motion = motion.normalized() * MOTION_SPEED
 
 	move_and_slide(motion)
+
+func decideAnimation(motion):
+	if motion == Vector2(0, 0):
+		var oldAnimation = $RunSprite/AnimationPlayer.current_animation;
+		var newAnimation = oldAnimation.replace('Run','Idle');
+		$RunSprite.hide();
+		$IdleSprite.show();
+		$IdleSprite/AnimationPlayer.play(newAnimation);
+	else:
+		$IdleSprite.hide();
+		$RunSprite.show();
+	
+	if motion == Vector2(1, 0):
+		$RunSprite/AnimationPlayer.play("RunRight");
+	if motion == Vector2(-1, 0):
+		$RunSprite/AnimationPlayer.play("RunLeft");
+	if motion == Vector2(0, 1):
+		$RunSprite/AnimationPlayer.play("RunDown");
+	if motion == Vector2(0, -1):
+		$RunSprite/AnimationPlayer.play("RunUp");
+	if motion == Vector2(1, 1):
+		$RunSprite/AnimationPlayer.play("RunDownRight");
+	if motion == Vector2(-1, 1):
+		$RunSprite/AnimationPlayer.play("RunDownLeft");
+	if motion == Vector2(1, -1):
+		$RunSprite/AnimationPlayer.play("RunUpRight");
+	if motion == Vector2(-1, -1):
+		$RunSprite/AnimationPlayer.play("RunUpLeft");
