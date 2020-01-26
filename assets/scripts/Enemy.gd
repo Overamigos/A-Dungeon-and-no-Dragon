d6e00c76f49937c38d0d25f0993f03ab7bd1459b
Rafael Xavier <rafael_ldsx@hotmail.com>
2020-01-26 18:19:15 -0300

    basic pathfinding

--- assets/scripts/Enemy.gd
+++ assets/scripts/Enemy.gd
@@ -5,6 +5,7 @@ extends KinematicBody2D
 var speed = 60
 var hp = 1
 var target
+var points = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +16,20 @@ func _process(delta):
 	if hp <=0:
 		queue_free()
 	
-	if(get_node("../../Player")):
-		var distance = (get_node("../../Player").global_position - global_position).normalized()
+	var enemy_position = get_global_position()
+	
+	points = get_node("../../Navigation2D").get_simple_path(enemy_position, get_node("../../Player").global_position, false)
+	
+	if points.size() > 1:
+		var distance = points[1] - enemy_position
+		var direction = distance.normalized()
+		if points.size() > 2:
+			move_and_slide(speed*direction)
+		else:
+			move_and_slide(Vector2(0,0))
+	
+	#if(get_node("../../Player")):
+	#	var distance = (get_node("../../Player").global_position - global_position).normalized()
 
-		move_and_slide(speed*distance)
+	#	move_and_slide(speed*distance)
 	pass
