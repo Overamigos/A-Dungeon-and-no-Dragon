94d00af32015fd3d45e2589c2a93955f81c218dd
Márcio Medeiros <marciomed07@gmail.com>
2020-02-02 12:22:06 -0300

    Folder system reorganized and throwing old trash away

--- assets/scripts/Powers/Timer.gd
+++ assets/scripts/Powers/Timer.gd
@@ -5,7 +5,7 @@ func _ready():
 
 func _on_Timer_timeout():
 	print('a')
-	var enemy = preload("res://assets/scenes/Enemy.tscn").instance()
+	var enemy = preload("res://assets/scenes/Characters/Enemy.tscn").instance()
 	get_parent().add_child(enemy)
 	print(enemy)
 	enemy.global_position = Vector2(0,0)
